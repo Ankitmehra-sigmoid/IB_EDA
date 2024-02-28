69db23334203e787b99001c872acfae563d8b594
Ankitmehra-sigmoid <77790923+Ankitmehra-sigmoid@users.noreply.github.com>
2024-02-28 17:41:15 +0530

    Update EDA_streamlit.py

--- EDA_streamlit.py
+++ EDA_streamlit.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 ############# Reading data and preprocessing######
-df_in=pd.read_csv('Cargosoft-Data_2022-2024.csv',encoding='ISO-8859-1')
+df_in=pd.read_csv('Cargosoft-Data_2022-2024.csv',encoding='mixed')
 df_in['Creation Date'] = pd.to_datetime(df_in['Creation Date'])
 df_in=standerdize_cols(df=df_in,selected_cols=['Place of delivery','Place of loading'])
 df_in['year']=df_in['Creation Date'].dt.year
